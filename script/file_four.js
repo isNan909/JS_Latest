0bdf3a1443452c6285aa0cc1bc12ed9a8f79bff3
Ishan Manandhar <ishan02016@gmail.com>
2020-04-08 19:56:47 +0545

    added triming method

---
+++ script/file_four.js
@@ -0,0 +1,14 @@
+// String.trimStart() & String.trimEnd()
+
+// const message = `      I Love Javascript      `;
+// console.log(message.trimStart());
+
+
+
+// const message = `      I Love Javascript  `;
+// console.log(message.trimEnd());
+// console.log(message.trimLeft());
+// console.log(message.trimRight());
+
+
+// console.log(message.length);
