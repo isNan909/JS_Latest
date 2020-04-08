3fb34c251178223fc56e223906ecaa03eeb40fdb
Ishan Manandhar <ishan02016@gmail.com>
2020-04-08 18:04:23 +0545

    file_one for flatenning the array

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # JS_Latest
 Latest features introduced in Javascript in ES10 and 11
+
+// ES10 
+
+<!-- file_one: Array.flat()
+file_two: Array.flatMap()
+file_three: Object.fromEntries
+file_four: String.trimStart() & String.trimEnd()
+file_five: Optional Catch Binding
+file_six: toString()
+file_seven: JSON.stringify()
+file_eight: Array.Sort Stability
+file_nine: Symbol.description
+file_ten: Object.fromEntries() -->
+
---
+++ script/file_one.js
@@ -0,0 +1,21 @@
+//Array.flat()//
+
+
+// const launguages = ['python', 'go', ['javascript', 'dart', ['coffeescript', 'webassembly']]];
+// console.log(launguages.flat(0));
+
+
+
+// const launguages = ['python', 'go', , , ['javascript', 'dart', ['coffeescript', 'webassembly']]];
+// console.log(launguages.flat(1));
+
+
+// const launguages = ['python', 'go', ['javascript', 'dart', ['coffeescript', 'webassembly', ['rust']]]];
+// console.log(launguages.flat(2));
+
+
+// const launguages = ['python', 'go', , ,  ['javascript', 'dart', ['coffeescript', 'webassembly', ['rust']]]];
+// console.log(launguages.flat(Infinity));
+
+
+
