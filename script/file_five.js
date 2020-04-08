6ea80142a2d557fa68d88c9b159b32a3aee23a94
Ishan Manandhar <ishan02016@gmail.com>
2020-04-08 20:06:49 +0545

    added optional catch bindings

---
+++ script/file_five.js
@@ -0,0 +1,16 @@
+// Optional Catch Binding
+
+// try{
+//     throw new Error("some error occured");
+// }
+// catch(e){
+//     console.log(e)
+// }
+
+
+// try{
+//     throw new Error("some error occured");
+// }
+// catch{
+//     console.log('no parameters passed')
+// }
