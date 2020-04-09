88dec4883b5cb1d33cdf61d22515f73fc9927d2b
Ishan Manandhar <ishan02016@gmail.com>
2020-04-09 09:39:46 +0545

    added sorting array feature

---
+++ script/file_eight.js
@@ -0,0 +1,19 @@
+// Sorting Array
+
+// const countries=[
+//     {name: "Mexico", count:1},
+//     {name: "Japan", count:20},
+//     {name: "Nepal", count:19},
+//     {name: "Russia", count:3},
+//     {name: "Bhutan", count:4},
+//     {name: "USA", count:10},
+//     {name: "Canada", count:15}
+// ];
+
+// let sort = (x,y) => x.count - y.count;
+// let sorted = countries.sort(sort);
+// console.log(sorted)
+
+
+// const sortAnother= countries.sort((x,y)=>x.count-y.count);
+// console.log(sortAnother)
